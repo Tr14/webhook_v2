ba252c7000a01760bcf842a27d1a111d02ea086b
Tr14 <xuantrucx111@gmail.com>
2023-11-08 16:37:00 +0700

    add webhook v2

--- webhook.js
+++ webhook.js
@@ -69,7 +69,7 @@ app.get('/facebook-authorization', (req, res) => {
     };
 
     let res_pagetoken = await axios(config_pagetoken);
-    page_access_token = res_pagetoken.data;
+    page_access_token = res_pagetoken.data.data[0].access_token;
   };
 
   fetchData().then(() => {
