fd4115822d6637360ffa42322709a6cc878751c4
Tr14 <xuantrucx111@gmail.com>
2023-11-08 10:43:41 +0700

    add webhook v2

--- webhook.js
+++ webhook.js
@@ -2,25 +2,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 1337; // Use the port of your choice
-var moment = require('moment-timezone');
 
 const VERIFY_TOKEN = 'lmaoez1234';
-
-
-var gmt7moment = moment().tz("GMT+07").toString();
-const regex = /[Moment<>]+/g;
-var gmt7timestamp = gmt7moment.replace(regex, '');
 
 // Middleware to parse incoming JSON data
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
+  require("log-timestamp")
+  console.log("\u001b[1;32m" + "dev.akadigital.net: " + "\u001b[0m" + hubChallenge)
   res.status(200).send("Welcome to AKA webhook");
 });
 
 app.get('/webhook', (req, res) => {
   const hubChallenge = req.query['hub.challenge'];
-  console.log("\u001b[1;32m" + "[" + gmt7timestamp + "] " + "\u001b[0m" + hubChallenge)
+  require("log-timestamp")
+  console.log("\u001b[1;32m" + "Hub Challenge: " + "\u001b[0m" + hubChallenge)
   res.status(200).send(hubChallenge);
   /*Use the verification token you set during webhook setup
   const hubMode = req.query['hub.mode'];
@@ -55,5 +52,6 @@ app.post('/webhook', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log("\u001b[1;32m" + "[" + gmt7timestamp + "] " + "\u001b[0m" + `Facebook Messenger webhook is running on port ${port}`);
+  require("log-timestamp")
+  console.log("\u001b[1;32m" + "Node: " + "\u001b[0m" + `Facebook Messenger webhook is running on port ${port}`);
 });
