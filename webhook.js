257aa8e5896adbcd9767e5fa49baf4ddfdf4c2c3
Tr14 <xuantrucx111@gmail.com>
2023-11-08 16:15:36 +0700

    add webhook v2

--- webhook.js
+++ webhook.js
@@ -47,12 +47,11 @@ app.get('/facebook-authorization', (req, res) => {
 
   fetchUserToken().then(() => {
     // You can access responseData here, once the request has completed
-    console.log('Data outside async function:', user_access_token);
+    console.log("\u001b[1;32m" + "User Access Token: " + "\u001b[0m", user_access_token);
+    res.send({ authorization_code: facebook_authorization_code, user_access_token: user_access_token });
   });
 
-
   res.sendFile(path.join(__dirname, 'public', '/authorization.html'));
-  res.send({ authorization_code: facebook_authorization_code, user_access_token: user_access_token });
 });
 
 app.get('/webhook', (req, res) => {
