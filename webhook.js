2a3d2db119a07b2b125e0356a5b458ba8d94869e
Tr14 <xuantrucx111@gmail.com>
2023-11-14 16:55:46 +0700

    update facebook webhook

--- webhook.js
+++ webhook.js
@@ -144,6 +144,7 @@ app.get('/webhook', (req, res) => {
 
 app.post('/webhook', (req, res) => {
   console.log("\u001b[1;32m" + "Data: " + "\u001b[0m" + 'Facebook request body:', req.body);
+  console.log("\u001b[1;32m" + "Data: " + "\u001b[0m" + 'Facebook request body detail:', req.body.entry[0].changes);
   received_updates.unshift(req.body);
   res.sendStatus(200);
 });
